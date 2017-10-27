38bc758c73be428fd659a1463fc8c135c25fd3a6
Jnb2387 <jnb2387@gmail.com>
2017-10-26 21:45:40 -0600

    first

--- README.md
+++
@@ -1,6 +0,0 @@
-node-web-scraper
-================
-
-Simple web scraper to get a movie name, release year and community rating from IMDB.
-"# json" 
-"# json" 
--- server.js
+++ server.js
@@ -1,35 +1,53 @@
-var express = require('express');
-var fs      = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app     = express();
+var express = require("express");
+var fs = require("fs");
+var request = require("request");
+var app = express();
+var rp = require("request-promise");
+var Promise = require("bluebird");
 
-app.get('/scrape', function(req, res){
-  // url = 'http://services7.arcgis.com/WvgOaBfZeRNlBq3y/ArcGIS/rest/services/CS_Trails/FeatureServer/0/query?where=OBJECTID+BETWEEN+15000+AND+20000&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=';
-  // url2 = 'http://services7.arcgis.com/WvgOaBfZeRNlBq3y/ArcGIS/rest/services/CS_Trails/FeatureServer/0/query?where=OBJECTID+BETWEEN+20000+AND+25000&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=';
-  url3 = 'http://services7.arcgis.com/WvgOaBfZeRNlBq3y/ArcGIS/rest/services/CS_Trails/FeatureServer/0/query?where=OBJECTID+BETWEEN+25000+AND+30000&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=';
-  
-  request(url3, function(error, response, html){
-    if(!error){
-      var $ = cheerio.load(html);
-    }
-    fs.writeFile('../trails/trails25kto30k.json', html, function(err){
-      console.log('20k done');
-    })
 
-    res.send('Check your console!')
-  })
-  // request(url2, function(error, response, html){
-  //   if(!error){
-  //     var $ = cheerio.load(html);
-  //   }
-  //   fs.writeFile('../trails/trails20kto25k.json', html, function(err){
-  //     console.log('30k done');
-  //   })
+app.get("/scrape", function(req, res) {
+  console.log("getting the data");
+  var total = 29990;
+  var x = 0;
+  var y = 5000;
+  var data = [];
+  while (x < total) {
+    // console.log("start x : " + x + "       start y : " + y);
+    url =
+      "http://services7.arcgis.com/WvgOaBfZeRNlBq3y/ArcGIS/rest/services/CS_Trails/FeatureServer/0/query?where=OBJECTID+BETWEEN+" +
+      x +
+      "+AND+" +
+      y +
+      "&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=";
+    rp(url)
+      .then(function(htmlString) {
+        console.log("Request Sent");
+       data.push(htmlString);
+        console.log(data.length)
+        var files = [];
+        for (var i = 0; i < data.length; ++i) {
+            files.push( fs.writeFileSync("../trails/trail" + i  + ".geojson", data[i], function(err) {
+              console.log("data appended");          
+            }));
+        }
+        Promise.all(files).then(function() {
+            console.log("all the files were created");
+        });
+      })
+      .catch(function(err) {
+        console.log("shit fucked up", err);
+      });
+    y = y + 5000;
+    x = x + 5000;
+    // console.log("End   x : " + x + "       End y : " + y);
+    
+  }
+ 
 
-  // })
-})
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
+  res.send("SCRAPEDDD");
+});
+app.listen("8081");
+console.log("Running on 8081");
 exports = module.exports = app;
