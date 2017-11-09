a834d84d67cd0ecba8b0dc126b16b346f4477abb
jeffrey bradley <jnb2387@gmail.com>
2017-11-09 07:59:59 -0700

    other changes

--- server.js
+++ server.js
@@ -8,39 +8,46 @@ var Promise = require("bluebird");
 
 app.get("/scrape", function(req, res) {
   console.log("getting the data");
-  var total = 29990;
+  var total = 25176;
   var x = 0;
-  var y = 5000;
+  var y = 1000;
   var data = [];
   while (x < total) {
-    // console.log("start x : " + x + "       start y : " + y);
-    url =
-      "http://services7.arcgis.com/WvgOaBfZeRNlBq3y/ArcGIS/rest/services/CS_Trails/FeatureServer/0/query?where=OBJECTID+BETWEEN+" +
-      x +
-      "+AND+" +
-      y +
-      "&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=";
-    rp(url)
+    console.log("start x : " + x + "       start y : " + y);
+
+    kyurl="http://services1.arcgis.com/dpmGqj7FxlwlvK0y/ArcGIS/rest/services/ParcelswithOwnership/FeatureServer/0/query?where=OBJECTID+BETWEEN+    " +
+    x +
+    "+AND+" +
+    y + "&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="
+    // url =
+    //   "http://services7.arcgis.com/WvgOaBfZeRNlBq3y/ArcGIS/rest/services/CS_Trails/FeatureServer/0/query?where=OBJECTID+BETWEEN+" +
+      // x +
+      // "+AND+" +
+      // y +
+    //   "&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=";
+    rp(kyurl)
       .then(function(htmlString) {
-        console.log("Request Sent");
-       data.push(htmlString);
-        console.log(data.length)
+        data.push(htmlString);
+        console.log("Request # " + data.length + " Sent");
         var files = [];
         for (var i = 0; i < data.length; ++i) {
-            files.push( fs.writeFileSync("../trails/trail" + i  + ".geojson", data[i], function(err) {
-              console.log("data appended");          
+            files.push( fs.writeFileSync("../scraped/scottcounty" + i  + ".geojson", data[i], function(err) {
+                        
             }));
         }
         Promise.all(files).then(function() {
-            console.log("all the files were created");
+          console.log("GeoJSON Written");
         });
       })
       .catch(function(err) {
         console.log("shit fucked up", err);
       });
-    y = y + 5000;
-    x = x + 5000;
-    // console.log("End   x : " + x + "       End y : " + y);
+    y = y + 1000;
+    x = x + 1000;
+    console.log("End   x : " + x + "       End y : " + y);
+    if(y > total){
+      console.log("FINISHED")
+    }
     
   }
  
